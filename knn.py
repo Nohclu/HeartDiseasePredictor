b95995d87052e961533834d73a733baee0b733d8
Nohclu <connormulready@gmail.com>
2019-10-07 10:57:46 +0100

    Fix: Rewrote using sklearn

--- knn.py
+++ knn.py
@@ -1,69 +1,36 @@
 import math, pandas, numpy
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report, confusion_matrix
 
-def classifyAPoint(points, p, k=3):
 
-    distance = []
-    for group in points:
-        for attr in points[group]:
-            euclidean_distance = numpy.linalg.norm(attr - p)
-            distance.append((euclidean_distance, group))
-
-    distance = sorted(distance)[:k]
-
-    freq1 = 0
-    freq2 = 0
-
-    for d in distance:
-        if d[1] == 0:
-            freq1 += 1
-        elif d[1] == 1:
-            freq2 += 1
-
-    return 0 if freq1 > freq2 else 1
+# def classifyAPoint(points, p, k=3):
 
 def main():
-    points = {
-        0: [],
-        1: []}
-    
-    test = {
-        0: [],
-        1: []
-    }
-
-# 160 1's
-# 136 0's
     df = pandas.read_csv('./datamining.csv', skiprows=[0, 1])
+    #  Randomize order of rows
     df = df.sample(frac=1)
-    hasHeartDisease = df["target"]
-    df = ((df - df.min()) / (df.max() - df.min()))
-    df["target"] = hasHeartDisease
 
-    print(df)  
-    for row_index, row in df.iterrows():
-        if row_index < 250:
-            if (row["target"] == 0):
-                points[0].append(numpy.array(df.iloc[row_index, 0:13]))
-            elif (row["target"] == 1):
-                points[1].append(numpy.array(df.iloc[row_index, 0:13]))
-        else:
-            if (row["target"] == 0):
-                test[0].append(numpy.array(df.iloc[row_index, 0:13]))
-            elif (row["target"] == 1):
-                test[1].append(numpy.array(df.iloc[row_index, 0:13]))
+    attr = df.iloc[:,:-1].values
+    classes = df.iloc[:,13].values
 
-    totalTestValues = len(test[0]) + len(test[1])
-    testPass = 0
-    k = 9
+    # Split data 80% 20%
+    X_train, X_test, y_train, y_test = train_test_split(attr, classes, test_size=0.20)
+    scaler = StandardScaler().fit(X_train)
 
-    for t in test[0]:
-        if classifyAPoint(points, t, k) == 0:
-            testPass += 1
-    for t in test[1]:
-        if classifyAPoint(points, t, k) == 1:
-            testPass += 1
+    X_train = scaler.transform(X_train)
+    X_test = scaler.transform(X_test)
 
-    print("Pass: "+str(testPass) + "\nTotal: "+ str(totalTestValues))    
+    classifier = KNeighborsClassifier(n_neighbors=7)
+    classifier.fit(X_train, y_train)
+
+    y_pred = classifier.predict(X_test)
+
+    print(classification_report(y_test, y_pred))
+
+
+    
 
 
 if __name__ == '__main__':
