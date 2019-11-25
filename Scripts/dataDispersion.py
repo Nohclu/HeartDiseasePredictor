44f03a9363d6e852f3b7af90c4dfc517fd763de3
Nohclu <connormulready@gmail.com>
2019-11-25 18:14:39 +0000

    Add: Data Dispersion using boxplotes & five number summary

---
+++ Scripts/dataDispersion.py
@@ -0,0 +1,12 @@
+import pandas
+import numpy as np
+import matplotlib.pyplot as plt
+
+df = pandas.read_csv('../dirtyHeart.csv')
+df = pandas.DataFrame(df)
+
+CONTINUOUS_HEADERS = ['age', 'trestbps', 'chol', 'thalach', 'oldpeak', 'ca']
+
+for col in CONTINUOUS_HEADERS:
+    df.boxplot(column=[col], return_type='axes');
+    plt.show()  
