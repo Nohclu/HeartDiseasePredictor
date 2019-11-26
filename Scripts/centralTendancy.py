3ecff3e9fa269dfda2565f2b5127b8700d7dbd22
Nohclu <connormulready@gmail.com>
2019-11-26 21:57:55 +0000

    Update: Remove savefig as it saved aggregated plots

--- Scripts/centralTendancy.py
+++ Scripts/centralTendancy.py
@@ -15,8 +15,8 @@ fontMedian = {'color':  'green',
         }
 
 
-CATEGORICAL_HEADERS = ['sex', 'cp', 'fbs', 'restecg', 'exang', 'slope', 'thal', 'target']
-CONTINUOUS_HEADERS = ['age', 'trestbps', 'chol', 'thalach', 'oldpeak', 'ca']
+CATEGORICAL_HEADERS = ['sex', 'cp', 'fbs', 'restecg', 'exang', 'slope', 'thal', 'target', 'ca']
+CONTINUOUS_HEADERS = ['age', 'trestbps', 'chol', 'thalach', 'oldpeak']
 
 for col in CONTINUOUS_HEADERS:
     bins = len(df[col].unique())
@@ -29,8 +29,8 @@ for col in CONTINUOUS_HEADERS:
     plt.text(df[col].median(), plt.ylim()[1]*0.8, 'Median: {}'.format(df[col].median()), fontdict=fontMedian)
     yVals = y.tolist()
     patches[yVals.index(max(yVals))].set_color('y')
-    # plt.show()
-    plt.savefig("..//CentralTendancyResults//hist"+col+".png")
+    plt.show()
+    # plt.savefig("..//CentralTendancyResults//hist"+col+".png")
 
 for col in CATEGORICAL_HEADERS:
 	d = {}
@@ -49,5 +49,5 @@ for col in CATEGORICAL_HEADERS:
 	
 	plt.axvline(df[col].median(), color='green', linestyle='dashed', linewidth=2)
 	plt.text(df[col].median(), plt.ylim()[1]*0.8, 'Median: {}'.format(df[col].median()), fontdict=fontMedian)
-	# plt.show()
-	plt.savefig("..//CentralTendancyResults//bar"+col+".png")
+	plt.show()
+	# plt.savefig("..//CentralTendancyResults//bar"+col+".png")
